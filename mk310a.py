bc7ea9ef515ce19e402ae2226765a16833a44589
Mafia-killer8080 <96686213+Mafia-killer8080@users.noreply.github.com>
2022-06-23 22:55:03 +0500

    Create mk310a.py

---
+++ mk310a.py
@@ -0,0 +1,19 @@
+import platform
+import os
+os.system('termux-setup-storage')
+#os.system('git pull')
+#try:os.system('mkdir /sdcard/mk310')
+#except:pass
+#try:os.system('mkdir /sdcard/mk310/OK')
+#except:pass
+#try:os.system('mkdir /sdcard/mk310/CP')
+#except:pass
+try:os.system('touch .prox.txt')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+        __import__("mk310a")._site_view_
+elif 'aarch' in arc:
+        __import__("mk310a")._site_view_()
+else:
+        exit(f' Unknow device machine {arc}')
